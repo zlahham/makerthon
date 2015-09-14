6f8f0b2cf29896f6144ebff15d588060bf98ec95
Zaid Al Lahham <zlahham@gmail.com>
2015-09-14 14:21:17 +0100

    sign up test passed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
+  resource :votes
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: "votes#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
