0a5a253bea06b2306a0e5a85ed7ba78970bb86fb
Zaid Al Lahham <zlahham@gmail.com>
2015-09-16 11:59:49 +0100

    FIxed

--- app/assets/javascripts/viewer.js
+++ app/assets/javascripts/viewer.js
@@ -1,26 +1,5 @@
-// Enable pusher logging - don't include this in production
-// Pusher.log = function(message) {
-//   if (window.console && window.console.log) {
-//     window.console.log(message);
-//   }
-// };
-
-// var pusher = new Pusher('da575b9edde9ebf3a600', {
-//   encrypted: true
-// });
-
-// var channel = pusher.subscribe('voting');
-// channel.bind('my_event', function(data) {
-//   // alert(data.upvote);
-//   var up = document.getElementById("upvote");
-//   var down = document.getElementById("downvote");
-//   up.innerHTML = data.upvote;
-//   down.innerHTML = data.downvote;
-
-// });
-
 // dev key for pusher
-// da575b9edde9ebf3a600
+// development da575b9edde9ebf3a600
 // production 9a2f3193a0af5c40e9c0
 
 
@@ -30,7 +9,7 @@
     }
   };
 
-  var pusher = new Pusher('9a2f3193a0af5c40e9c0', {
+  var pusher = new Pusher('da575b9edde9ebf3a600', {
     encrypted: true
   });
 
